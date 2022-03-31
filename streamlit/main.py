e8cf873eb0697a61b846eb8306bfed93eabe28fc
matin-n <40713017+matin-n@users.noreply.github.com>
2022-03-30 19:54:21 -0700

    Predict first fight in Streamlit dashboard
    Loads the trained VotingClassifier model and cleaned dataset with Joblib.

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,29 +1,40 @@
+import os
+
+import numpy as np
 import pandas as pd
+import psycopg2
+from joblib import load
+
 import streamlit as st
 
-def print_hi():
-    df = pd.read_csv("../Resources/clean_scraped_data.csv")
-    col_left, col_mid, col_right = st.columns(3)
-
-    with col_left:
-        fighter_one_dropbox = st.selectbox(
-            "Select Fighter 1",
-            ("Test", "Test2",)
-        )
-        st.write("you selected", fighter_one_dropbox)
-
-    with col_mid:
-        st.write("test")
-
-    with col_right:
-        fighter_two_dropbox = st.selectbox(
-            "Select Fighter 2",
-            ("Test2", "Test1",)
-        )
-        st.write("you selected", fighter_two_dropbox)
+# Load model. # TODO: Add other classifiers.
+def load_model(classifier="../Resources/clf.joblib"):
+    return load(os.path.join(classifier))
 
 
+# Predict who wins the fight.
+def predict(df):
+    # If dataframe column contains Winner then drop it
+    if "Winner" in df.columns:
+        df = df.drop(columns=["Winner"])
+    return clf.predict(df), clf.predict_proba(df)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi()
+
+# Load Model
+clf = load_model()
+
+# Load DataFrame
+ufc_df = load("../Resources/clean_scraped_data.joblib")
+
+# Main page
+# -------------------------#
+st.title("UFC Fighter Prediction")
+# Predict fight based on fighter stats
+st.header("Predict Fight")
+
+# Predict first fight
+fight_selection = ufc_df.iloc[[0]]
+prediction, pred_proba = predict(fight_selection)
+st.write(prediction)
+st.write(pred_proba)
+
